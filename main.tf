61702911c9f332b6a6d511932b6e3eab7db7f498
saivamsisuru <78493871+saivamsisuru@users.noreply.github.com>
2022-11-16 12:28:38 +0530

    newkjahdeluh

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws"  {
+    region= "us-west-2" 
+    access_key= "AKIAW5D4SVQGFKS3GEEH"
+    secret_key= "MmGSzJX+RBpVlffwySmwLZwYuZjsn3SDsoEOQh1J"
+}
+
+resource "aws_instance" "example" {
+   count = 2
+   ami = "ami-0c09c7eb16d3e8e70"
+   type = "t2.micro"
+
+}
